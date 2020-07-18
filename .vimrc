d805211dc2afa5aad16af812969192d8e9aa8f44
datousir <datousir@gmail.com>
2020-07-18 14:50:32 +0800

    update configs

--- .vimrc
+++ .vimrc
@@ -1,12 +1,14 @@
 " https://vi.stackexchange.com/questions/2572/detect-os-in-vimscript
 if has('mac')
     " let g:python_host_prog = '/usr/local/bin/python2'
-    let g:python_host_prog = '/Users/datousir/.pyenv/shims/python'
+    let g:python_host_prog = '/Users/datousir/.pyenv/shims/python2'
     " let g:python3_host_prog = '/usr/local/bin/python3'
-    let g:python_host_prog = '/Users/datousir/.pyenv/shims/python3'
+    let g:python3_host_prog = '/Users/datousir/.pyenv/shims/python3'
 elseif has('unix') " for linux
-    let g:python_host_prog = '/usr/bin/python'
-    let g:python3_host_prog = '/usr/bin/python3'
+    " let g:python_host_prog = '/usr/bin/python'
+    let g:python_host_prog = '/home/datousir/.pyenv/shims/python2'
+    " let g:python3_host_prog = '/usr/bin/python3'
+    let g:python3_host_prog = '/home/datousir/.pyenv/shims/python3'
 endif
 
 set runtimepath+=~/.vim_runtime
--- prepare_env.sh
+++ prepare_env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# install amd rx560 driver
+# https://www.amd.com/en/support/graphics/radeon-500-series/radeon-rx-500-series/radeon-rx-560
 
 # sudo apt install zsh
 
@@ -7,8 +9,11 @@
 
 # logout and logback
 
+# install sogou input method
+# https://gitee.com/laomocode/fcitx-sogouimebs
 
-sudo apt-get install tmux curl python2 python3 neovim universal-ctags global doxygen id-utils python3-pygments
+sudo apt-get install tmux curl python2 python3 neovim universal-ctags global \
+  doxygen id-utils python3-pygments
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -19,6 +24,8 @@ sh ~/.vim_runtime/install_basic_vimrc.sh
 
 # install rustup
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+cargo install ripgrep fd-find lsd exa
 
 # install pyenv
 # https://github.com/pyenv/pyenv/wiki/Common-build-problems
@@ -32,6 +39,27 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 
 python2 get-pip.py
 python3 get-pip.py
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+nvm install --lts
+
+# vim-plug
+
+# vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# install nerd font
+# install jetbrains nerd font
+
+# install powerlevel10k
+git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 
 # ====================== update config ====================================
 SRC_DIR=$(pwd)
